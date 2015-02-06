46fe18f9c5e284fb9d372ccee8bcc82c9f9db6ad
KeenenCharles <kcreddevils@gmail.com>
2015-02-06 07:30:28 -0400

    Server uses flask

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-from twisted.web.server import Site       #import twisted stuff
-from twisted.web.resource import Resource
-from twisted.internet import reactor
-from twisted.web.static import File
+from flask import Flask
+from flask.ext import restful
 from bson import json_util
 import genetic
+
+app = Flask(__name__)
+api = restful.Api(app)
 
 from parse_rest.connection import register
 register("To6ZjPMq8BUtsCyUDGHSG8BtwVfSpNJ3XjpxdMOh", "g90vPDRIVQZp1Pc8VlaC8ekEH3rTDvY4vXHuFbGF", master_key = "PD2mJf48vAaItVo5NdCWYeuCArAPgOsoOHWY4s6V")
@@ -13,17 +14,16 @@ from parse_rest.datatypes import Object
 class Crop(Object):
     pass
 
-class evaluateAPI(Resource):
-  def render_GET(self, request):
-      if 'size' in request.args:
-          crops = Crop.Query.all()
-          results = genetic.gen(crops, request.args['size'])
-          return json_util.dumps(results)
+class Magic(restful.Resource):
+    def get(self, soil, size):
+        if soil and size:
+            print size
+            print soil
+            crops = Crop.Query.filter(soil=soil)
+            results = genetic.gen(crops, size, soil)
+            return json_util.dumps(results)
 
-root = File("lampwww")               #Create a root
-root.putChild("magic", evaluateAPI())     #Create a child that will handle requests (the second argument must be the class name)
-factory = Site(root)             #Initialize the twisted object
-reactor.listenTCP(9080, factory)   #Choose the port to listen on (80 is standard for HTTP)
-                               #so don't bother putting anything after it
+api.add_resource(Magic, '/magic/<string:soil>/<string:size>')
 
-reactor.run()                         #Start listening, this command is an infinite loop
+if __name__ == '__main__':
+    app.run(debug=True)
